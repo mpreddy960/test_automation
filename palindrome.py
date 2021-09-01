5b3bf2b8f2ee48c2ff946d461a5e64bde09f2d32
malapati prathapreddy <mpreddy9505357960@gmail.com>
2021-09-01 10:48:14 +0530

    OS_module_practise

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+python_job = "reddy"
+print(python_job)
+coding = python_job[::-1]
+if python_job==coding:
+    print("it is palindrome")
+else:
+    print("it is not palindrome")
+
+
+palindrome_string = "121"
+print(palindrome_string)
+revese_string = palindrome_string[::-1]
+if (palindrome_string==revese_string):
+    print("palindrome")
+else:
+    print("not palidrome")
+
+
